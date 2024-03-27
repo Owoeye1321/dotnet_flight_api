b1e3b58c31d236e6178b7280a5ce2cad113150a0
Owoeye Samuel <owoeye1321@gmail.com>
2024-03-27 20:59:27 +0100

    PTSD 🔥💥🌪☠️🥶🔥🧑🏿‍💻🥳💥

--- Service/FlightService.cs
+++ Service/FlightService.cs
@@ -4,6 +4,37 @@ namespace FlightApi.Service
 {
   public class FlightService : IFlightAction
   {
-
+    public async Task<object> create(object payload)
+    {
+      return new {};
+    }
+      public async Task<object> pollSearch(string sessionToken)
+    {
+      
+    }
+      public async Task<object> autoSuggestFlight(object payload)
+    {
+      
+    }
+      public async Task<object> autoSuggestHotels(object payload)
+    {
+      
+    }
+      public async Task<object> searchHotels(object payload)
+    {
+      
+    }
+      public async Task<object> getLocals()
+    {
+      
+    }
+      public async Task<object> getCarrier()
+    {
+      
+    }
+      public async Task<object> getLocations(object payload)
+    {
+      
+    }
   }
 }
