84b3f3cd2dbd0ffc210e7329863a80bbe9f3adbc
Owoeye Samuel <owoeye1321@gmail.com>
2024-04-10 13:45:35 +0100

    dang! i am an introvert 🥶🥶🥶🥶☠️🕷🕷🕷🥷🏻

--- credential.sh
+++ credential.sh
@@ -1,4 +1,5 @@
 export XRapidAPIKey="37f4bc3034msha49c34d8cab9a70p1a537fjsnc2dbc343db77"
 export XRapidAPIHost="skyscanner80.p.rapidapi.com"
 export MongoUrl="mongo://localhost:27017" 
-export RapidUrl ="https://skyscanner"
+export RapidUrl ="https://skyscanner80.p.rapidapi.com/api/v1"
+
