eac98d75576bd3f9706d570cfcbbe5528d3a0046
Owoeye Samuel <owoeye1321@gmail.com>
2024-04-15 22:58:21 +0100

    more updates changes 🥶🥶🥶🥶

--- Controllers/FlightController.cs
+++ Controllers/FlightController.cs
@@ -7,8 +7,13 @@ public class FlightController : ControllerBase
   public FlightController(){}
   [HttpGet("/get-config")]
   public async Task<IActionResult> getConfig(){
-
   }
 
+ [HttpGet("/check-server-status")]
+  public async Task<IActionResult> checkServerStatus(){
+  }
+   [HttpGet("/auto-complete")]
+  public async Task<IActionResult> autoComplete(){
+  }
 }
 }
