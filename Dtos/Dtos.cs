54a72437da97a8c99aad46cf5908894625f0da86
Owoeye Samuel <owoeye1321@gmail.com>
2024-04-25 22:47:21 +0100

    started password update 🥳

--- Dtos/Dtos.cs
+++ Dtos/Dtos.cs
@@ -3,4 +3,5 @@ namespace FlightApi.Dtos
 {
   public record loginDto([Required] string email, [Required] string password);
   public record userDto([Required] string username, [Required] string email, [Required] string password);
+  public record forgetPasswordDto([Required] string email);
 }
