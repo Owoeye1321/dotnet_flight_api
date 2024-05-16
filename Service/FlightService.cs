1d33a6f9b76232ee85d7410d1578d3b19ee729d1
Owoeye Samuel <owoeye1321@gmail.com>
2024-05-16 21:07:41 +0100

    continuous development 💨

--- Service/FlightService.cs
+++ Service/FlightService.cs
@@ -73,9 +73,10 @@ namespace FlightApi.Service
           //   httpRequestMessage.Headers.Add("X-RapidAPI-Key","37f4bc3034msha49c34d8cab9a70p1a537fjsnc2dbc343db77");
           //   httpRequestMessage.Headers.Add("X-RapidAPI-Host","skyscanner80.p.rapidapi.com");
 
-           httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Key","37f4bc3034msha49c34d8cab9a70p1a537fjsnc2dbc343db77");
-          httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Host","skyscanner80.p.rapidapi.com");
-          var response = await httpClient.GetAsync("https://skyscanner80.p.rapidapi.com/api/v1/get-config");
+           httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Key",rapidApiKey);
+          httpClient.DefaultRequestHeaders.Add("X-RapidAPI-Host",rapidHost);
+          Console.WriteLine(rapidApiKey, rapidHost, configUrl);
+          var response = await httpClient.GetAsync($"{configUrl}/get-config");
           if(response.StatusCode != HttpStatusCode.OK){
             throw new UnprocessableEntityException($"{response.StatusCode} API Error");
           }
