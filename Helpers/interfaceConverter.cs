c2cae3bfbab8e24522f68ebf8d86d8df972f8a11
Owoeye Samuel <owoeye1321@gmail.com>
2024-05-21 21:41:56 +0100

    added more class interface converter

--- Helpers/interfaceConverter.cs
+++ Helpers/interfaceConverter.cs
@@ -1,29 +1,40 @@
 using FlightApi.Interface;
-namespace FlightApi.Helpers{
-  public class CheckServerStatus : ICheckServerStatus {
+namespace FlightApi.Helpers
+{
+  public class CheckServerStatus : ICheckServerStatus
+  {
     public bool status { get; set; }
     public string message { get; set; }
   }
 
-  public class getConfig: IGetConfig{
+  public class getConfig : IGetConfig
+  {
     public bool status { get; set; }
     public string message { get; set; }
     public double timestamp { get; set; }
     public IEnumerable<getConfigDataObject> data { get; set; }
   }
-  public class autoCompleteDataObject:IAutoCompleteDataObject {
-    public string id { get; set; }
-
+  public class autoCompleteDataObject : IAutoCompleteDataObject
+  {
+    public string id { get; set; };
+    public autoCompletePresentation presentation { get; set; };
+    public autoCompleteNavigation navigation { get; set; };
   }
 
-  public class autoComplete : IAutoComplete{
-     public bool status { get; set; }
+  public class autoCompletePresentation: IAutoCompletePresentation{
+    
+  }
+
+  public class autoComplete : IAutoComplete
+  {
+    public bool status { get; set; }
     public string message { get; set; }
-   public  IEnumerable<autoCompleteDataObject> data { get; set; }
+    public IEnumerable<autoCompleteDataObject> data { get; set; }
   }
 
-  public class getConfigDataObject: IGetConfigDataObject{
-     public string country { get; set; }
+  public class getConfigDataObject : IGetConfigDataObject
+  {
+    public string country { get; set; }
     public string market { get; set; }
     public string locale { get; set; }
     public object currencyTitle { get; set; }
