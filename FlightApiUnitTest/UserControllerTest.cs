f2c6088018e41328c8d73c7220f4f094c490e256
Owoeye Samuel <owoeye1321@gmail.com>
2024-06-01 14:11:22 +0100

    continuous testing 💨

--- FlightApiUnitTest/UserControllerTest.cs
+++ FlightApiUnitTest/UserControllerTest.cs
@@ -1,10 +1,33 @@
+using System;
+using Xunit;
+using Moq;
+using FlightApi.Dtos;
+using FlightApi.Interface;
+using FlightApi.Controllers;
+
 namespace FlightApiUnitTest;
 
 public class UserControllerTest
 {
     [Fact]
-    public void loginAsync()
+    public async Task loginAsync_withUnexistingItem_ReturnBadRequest()
     {
 
+        //Arrange
+        loginDto data = new() { email = "oweure", password = "jehhoij" };
+        var userServiceStub = new Mock<IUserAction>();
+        userServiceStub.Setup(repo => repo.loginAsync(data)).ReturnAsync(null);
+        var jwtService = new Mock<IJwtAction>();
+
+        var userController = new UserController(userServiceStub.Object, jwtService);
+
+
+        //Act 
+        var result = await userController.loginAsync(null);
+
+
+        //Assert
+        Assert.IsType<BadRequest>(result.Result);
+
     }
-} 
+}
