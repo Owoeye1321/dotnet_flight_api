78fdf0517eaa20905fca0b90d6c23da63e759596
Owoeye Samuel <owoeye1321@gmail.com>
2024-06-14 21:46:14 +0100

    no idea mehn!

--- FlightApi/Interface/IUser.cs
+++ FlightApi/Interface/IUser.cs
@@ -1,11 +1,22 @@
 
 namespace FlightApi.Interface
 {
-  public interface IUser{
-    Guid id {get; set;}
-    string username {get; set;}
-    string email {get; set;}
-    string password {get; set;}
-    string salt {get; set;}
+  public interface IUser
+  {
+    Guid id { get; set; }
+    string username { get; set; }
+    string email { get; set; }
+    string password { get; set; }
+    string salt { get; set; }
+  }
+  public interface Ilogin
+  {
+    string email { get; set; }
+    string password { get; set; }
+  }
+  public class login : Ilogin
+  {
+    public string email { get; set; }
+    public string password { get; set; }
   }
 }
--- FlightApiUnitTest/UserControllerTest.cs
+++ FlightApiUnitTest/UserControllerTest.cs
@@ -9,25 +9,27 @@ namespace FlightApiUnitTest;
 
 public class UserControllerTest
 {
+        private readonly IUserAction userService;
+        private readonly IJwtAction jwtService;
+    public UserController(){
+        //Dependencies
+        userService = 
+        
+        //Class
+        userController = new UserController()
+    }
+
     [Fact]
-    public async Task loginAsync_withUnexistingItem_ReturnBadRequest()
+    public void UserController_loginAsync_ReturnSuccess()
     {
 
         //Arrange
-        loginDto data = new() { email = "oweure", password = "jehhoij" };
-        var userServiceStub = new Mock<IUserAction>();
-        userServiceStub.Setup(repo => repo.loginAsync(data)).ReturnAsync(null);
-        var jwtService = new Mock<IJwtAction>();
-
-        var userController = new UserController(userServiceStub.Object, jwtService);
 
 
         //Act 
-        var result = await userController.loginAsync(null);
 
 
         //Assert
-        Assert.IsType<BadRequest>(result.Result);
 
     }
 }
